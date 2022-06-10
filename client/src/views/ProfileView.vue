54ce92dbb3b2c64a5e94c0733d84ce9826cda186
pierre.dhebrail@student-cs.fr <pierre.dhebrail@student-cs.fr>
2022-06-10 15:03:31 +0200

    ✨(profile-page): simple profile page

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="background">
     <div class="frame">
-      Yo
-      <p>Profile value is {{ this.profile }}</p>
+      <h1>{{profile.first_name}} {{profile.last_name}}</h1>
+      <p>Né le {{profile.birth_date}}</p>
+      <p>Mort le {{profile.death_date}}</p>
+      <p>{{profile.biography}}</p>
     </div>
   </div>
 </template>
@@ -33,11 +35,16 @@ export default {
 </script>
 
 <style scoped>
+
+h1 {
+  margin: 0;
+}
 .frame {
   background-color: var(--bg-color);
   width: 80%;
   margin: 1.5em auto;
-  border-radius: 0.5em;
+  padding: 1em;
+  border-radius: 0.8em;
   box-shadow: 0 0 0.5em #dddddd;
 }
 </style>
